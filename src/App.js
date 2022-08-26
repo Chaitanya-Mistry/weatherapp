65f357f1d37781598d535a687dd52d2e80a2444b
Chaitanya Mistry <ambicanagar17@gmail.com>
2022-08-26 13:22:39 -0400

    Basic structure is now ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="weatherMainContainer">
+        {/* User Input Container */}
+        <div id="userInputContainer">
+
+          {/* Main Title of our app */}
+          <h3 id="heading_of_app">my weather app</h3>
+          {/* Input box */}
+          <input type="text" id="userInputBox" placeholder="Type city name 🏙️ .." />
+          <span id="seperator">or</span>
+          {/* Use user's location(coordinates) */}
+          <button id="myLocationBtn">📡 your location </button>
+        </div>
+
+        {/* Weather Details */}
+        <div id="weatherDetails">
+            {/* Main weather details */}
+            <div id="mainWeatherDetails">
+              
+            </div>
+            {/* Extra weather details */}
+            <div id="extraWeatherDetails">
+
+            </div>
+        </div>
     </div>
   );
 }
