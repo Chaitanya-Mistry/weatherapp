8e916a41d10074241e1ce968a103a4461b1f7005
Chaitanya Mistry <ambicanagar17@gmail.com>
2022-08-26 15:22:31 -0400

    styling done

--- src/App.js
+++ src/App.js
@@ -3,29 +3,63 @@ import './App.css';
 function App() {
   return (
     <div id="weatherMainContainer">
-        {/* User Input Container */}
-        <div id="userInputContainer">
+      {/* User Input Container */}
+      <div id="userInputContainer">
 
-          {/* Main Title of our app */}
-          <h3 id="heading_of_app">my weather app</h3>
-          {/* Input box */}
-          <input type="text" id="userInputBox" placeholder="Type city name 🏙️ .." />
-          <span id="seperator">or</span>
-          {/* Use user's location(coordinates) */}
-          <button id="myLocationBtn">📡 your location </button>
+        {/* Main Title of our app */}
+        <h3 id="heading_of_app">
+          my weather app
+        </h3>
+        {/* Input box */}
+        <input type="text" id="userInputBox" placeholder="Type city name 🏙️ .." />
+
+        <span id="seperator">or</span>
+
+        {/* Use user's location(coordinates) */}
+        <button id="myLocationBtn">📡 your location </button>
+      </div>
+
+      {/* Weather Details */}
+      <div id="weatherDetails">
+
+        {/* Main weather details */}
+        <div id="mainWeatherDetails">
+          {/* Weather Icon */}
+
+          <span className='htmlEmojis'>🌤️</span>
+
+          {/* Temperature in celcius */}
+          <p id="temperature">
+            27<sup>o</sup>C
+          </p>
+          {/* Condition */}
+          <p id="condition">
+            Haze
+          </p>
+          {/* Location (City Name) */}
+          <p id="location">
+            🗺️ Mumbai
+          </p>
         </div>
 
-        {/* Weather Details */}
-        <div id="weatherDetails">
-            {/* Main weather details */}
-            <div id="mainWeatherDetails">
-              
-            </div>
-            {/* Extra weather details */}
-            <div id="extraWeatherDetails">
-
-            </div>
+        {/* Extra weather details */}
+        <div id="extraWeatherDetails">
+          {/* Feels like  */}
+          <div id="feelsLike">
+            <p>
+              🌡️ 32<sup>o</sup>C
+            </p>
+            <p>Feels Like</p>
+          </div>
+          {/* Humidity Section */}
+          <div id="humadity">
+            <p>
+              💧 83%
+            </p>
+            <p>Humadity</p>
+          </div>
         </div>
+      </div>
     </div>
   );
 }
