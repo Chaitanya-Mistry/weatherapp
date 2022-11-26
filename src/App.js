f6a866fd945060668146d26f9730de6da4ff7eb2
Chaitanya Mistry <chaitanyam688@gmail.com>
2022-11-26 10:53:12 -0500

    splash screen implemented ...

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { SplashScreen } from './SplashScreen';
 
 function App() {
   // States
-  const [city, setCity] = useState(null);  // To 
+  const [splashScreen, setSplashScreen] = useState(true);
+  const [city, setCity] = useState(null);
   const [showWeather, setShowWeather] = useState("disable");
   const [cityWeather, setCityWeather] = useState(null);
 
+  const displaySplashScreen = () => {
+    setTimeout(() => {
+      setSplashScreen(false);
+    }, 1000);
+  }
+
+  useEffect(() => {
+    displaySplashScreen();
+  }, []);
   // API Key for Open Weather Map API ( https://openweathermap.org/ )
-  const apiKey = "a03b46846e7b8b2caa42bdf0a96f1d1c";
+  const apiKey = process.env.REACT_APP_apiKey;
 
   // When user starts typing city name ✍️
   const loadCity = async (event) => {
@@ -23,6 +34,7 @@ function App() {
       if (response.status === 200 && response.statusText === "OK") {
         // Convert response in json format 
         const data = await response.json();
+
         setCity(data.name);
         setCityWeather(data);
         setShowWeather("enable");
@@ -49,7 +61,8 @@ function App() {
       1. The geolocation property is only available in secure contexts (HTTPS).
       2. The geolocation property is only available if the user approves it.
     */
-   
+
+    setSplashScreen(true);
     if (window.navigator.geolocation) // if this API is supported by the browser ❔
     {
       window.navigator.geolocation.getCurrentPosition(async (position) => {
@@ -62,9 +75,13 @@ function App() {
         if (response.status === 200 && response.statusText === "OK") {
           // Convert response in json format 
           const data = await response.json();
-          setCity(data.name);
-          setCityWeather(data);
-          setShowWeather("enable");
+          
+          setTimeout(() => {
+            setSplashScreen(false);
+            setCity(data.name);
+            setCityWeather(data);
+            setShowWeather(true);
+          }, 1000);  
         }
         // If status is not ok
         else {
@@ -72,99 +89,103 @@ function App() {
           setShowWeather("disable");
           setCityWeather(null);
         }
-      },(error)=> console.log(error));
+      }, (error) => console.log(error));
     }
   }
   return (
-    <div id="weatherMainContainer">
+    <>
+      {/* Splash screen component */}
+      {splashScreen ? <SplashScreen /> : ""}
+      <div id="weatherMainContainer">
 
-      {/* User Input Container */}
-      <div id="userInputContainer">
+        {/* User Input Container */}
+        <div id="userInputContainer">
 
-        {/* Main Title of our app */}
-        <h3 id="heading_of_app">
-          my weather app
-        </h3>
-        {/* Input box */}
-        <input type="text" id="userInputBox" onChange={loadCity} placeholder="Type city name 🏙️ .." />
+          {/* Main Title of our app */}
+          <h3 id="heading_of_app">
+            sky zone
+          </h3>
+          {/* Input box */}
+          <input type="text" id="userInputBox" onChange={loadCity} placeholder="Type city name 🏙️ .." />
 
-        <span id="seperator">or</span>
+          <span id="seperator">or</span>
 
-        {/* Use user's location(coordinates) */}
-        <button id="myLocationBtn" onClick={detectMyLocation}>📡 your location </button>
-      </div>
-
-      {/* Show weather details only when user entered correct city name */}
-      {!city ? (
-        <span></span>
-      ) : (
-        <div id="weatherDetails" className={showWeather}>
-
-          {/* Main weather details */}
-          <div id="mainWeatherDetails">
-            {/* Weather Icon */}
-
-            <span className='htmlEmojis'>
-              
-              {/* Overcast clouds */}
-              {cityWeather.weather[0].description === "overcast clouds" ? (<span className='htmlEmojis'> ☁️ </span>
-              ):(<span/>)}
-              {/* Few clouds or Scattered Clouds */}
-              {cityWeather.weather[0].description === "few clouds" || cityWeather.weather[0].description === "scattered clouds" ? (<span className='htmlEmojis'> 🌥️ </span>
-              ):(<span/>)}
-              {/* Sunny */}
-              {cityWeather.weather[0].description === "clear sky" ? (<span className='htmlEmojis'> ☀️ </span>
-              ):(<span/>)}
-              {/* Haze or Mist */}
-              {cityWeather.weather[0].description === "haze" || cityWeather.weather[0].description === 'mist' ? (<span className='htmlEmojis'> 🌁 </span>
-              ):(<span/>)}
-              {/* Broken Clouds */}
-              {cityWeather.weather[0].description === "broken clouds" ? (<span className='htmlEmojis'> 🌤️ </span>
-              ):(<span/>)}
-              {/* Rain */}
-              {(cityWeather.weather[0].description).includes("rain") ? (<span className='htmlEmojis'> ⛈️ </span>
-              ):(<span/>)}
-              {/* Snow */}
-              {(cityWeather.weather[0].description).includes("snow") ? (<span className='htmlEmojis'> 🌨️ </span>
-              ):(<span/>)}
-
-            </span>
-
-            {/* Temperature in celcius */}
-            <p id="temperature">
-              {cityWeather.main.temp}<sup>o</sup>C
-            </p>
-            {/* Condition */}
-            <p id="condition" style={{textTransform:'capitalize'}}>
-              {cityWeather.weather[0].description}
-            </p>
-            {/* Location (City Name) */}
-            <p id="location">
-              🗺️ {cityWeather.name} 
-            </p>
-          </div>
-        {console.log(cityWeather.weather[0].description)}
-          {/* Extra weather details */}
-          <div id="extraWeatherDetails">
-            {/* Feels like  */}
-            <div id="feelsLike">
-              <p>
-                🌡️ {cityWeather.main.feels_like}<sup>o</sup>C
-              </p>
-              <p>Feels Like</p>
-            </div>
-            {/* Humidity Section */}
-            <div id="humadity">
-              <p>
-                💧 {cityWeather.main.humidity}%
-              </p>
-              <p>Humadity</p>
-            </div>
-          </div>
+          {/* Use user's location(coordinates) */}
+          <button id="myLocationBtn" onClick={detectMyLocation}>📡 your location </button>
         </div>
-      )}
-      {/* Outer Div */}
-    </div>
+
+        {/* Show weather details only when user entered correct city name */}
+        {!city ? (
+          <span></span>
+        ) : (
+          <div id="weatherDetails" className={showWeather}>
+
+            {/* Main weather details */}
+            <div id="mainWeatherDetails">
+              {/* Weather Icon */}
+
+              <span className='htmlEmojis'>
+
+                {/* Overcast clouds */}
+                {cityWeather.weather[0].description === "overcast clouds" ? (<span className='htmlEmojis'> ☁️ </span>
+                ) : (<span />)}
+                {/* Few clouds or Scattered Clouds */}
+                {cityWeather.weather[0].description === "few clouds" || cityWeather.weather[0].description === "scattered clouds" ? (<span className='htmlEmojis'> 🌥️ </span>
+                ) : (<span />)}
+                {/* Sunny */}
+                {cityWeather.weather[0].description === "clear sky" ? (<span className='htmlEmojis'> ☀️ </span>
+                ) : (<span />)}
+                {/* Haze or Mist */}
+                {cityWeather.weather[0].description === "haze" || cityWeather.weather[0].description === 'mist' ? (<span className='htmlEmojis'> 🌁 </span>
+                ) : (<span />)}
+                {/* Broken Clouds */}
+                {cityWeather.weather[0].description === "broken clouds" ? (<span className='htmlEmojis'> 🌤️ </span>
+                ) : (<span />)}
+                {/* Rain */}
+                {(cityWeather.weather[0].description).includes("rain") ? (<span className='htmlEmojis'> ⛈️ </span>
+                ) : (<span />)}
+                {/* Snow */}
+                {(cityWeather.weather[0].description).includes("snow") ? (<span className='htmlEmojis'> 🌨️ </span>
+                ) : (<span />)}
+
+              </span>
+
+              {/* Temperature in celcius */}
+              <p id="temperature">
+                {cityWeather.main.temp}<sup>o</sup>C
+              </p>
+              {/* Condition */}
+              <p id="condition" style={{ textTransform: 'capitalize' }}>
+                {cityWeather.weather[0].description}
+              </p>
+              {/* Location (City Name) */}
+              <p id="location">
+                🗺️ {cityWeather.name}
+              </p>
+            </div>
+            {console.log(cityWeather.weather[0].description)}
+            {/* Extra weather details */}
+            <div id="extraWeatherDetails">
+              {/* Feels like  */}
+              <div id="feelsLike">
+                <p>
+                  🌡️ {cityWeather.main.feels_like}<sup>o</sup>C
+                </p>
+                <p>Feels Like</p>
+              </div>
+              {/* Humidity Section */}
+              <div id="humadity">
+                <p>
+                  💧 {cityWeather.main.humidity}%
+                </p>
+                <p>Humadity</p>
+              </div>
+            </div>
+          </div>
+        )}
+        {/* Outer Div */}
+      </div>
+    </>
   );
 }
 
---
+++ src/SplashScreen.js
@@ -0,0 +1,8 @@
+
+export const SplashScreen = () => {
+    return(
+        <div id="splashScreen">
+            <span id="splashContent">☀️</span>
+        </div>
+    )
+}
